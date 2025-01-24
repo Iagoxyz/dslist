1c936a89edefda7aec23245471913fde9522c77d
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-01-24 09:08:50 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -20,9 +20,18 @@ public class GameListController {
     @Autowired
     private GameListService service;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> list = service.findAll();
+        return list;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> list = gameService.findByList(listId);
         return list;
     }
 }
